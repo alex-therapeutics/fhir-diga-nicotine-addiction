6793533e24b6054fd919c55530bfb74b31783658
fongie <mkorlinge@hotmail.com>
2021-04-09 08:59:17 +0200

    add composition

---
+++ java-package/fhir/src/main/java/com/alextherapeutics/model/ExportedNicotineUsageTreatmentData.java
@@ -0,0 +1,9 @@
+package com.alextherapeutics.model;
+
+import com.alextherapeutics.annotation.GeneratedFromFhir;
+
+@GeneratedFromFhir(
+        id = "exported-nicotine-usage-treatment-data"
+)
+public interface ExportedNicotineUsageTreatmentData {
+}
